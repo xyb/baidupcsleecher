eeac2a0acdc8b7adcd25771ca0212f7944834f60
Xie Yanbo <xieyanbo@gmail.com>
2023-03-16 10:57:54 +0800

    chore: dockerize

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+python manage.py runserver 0.0.0.0:8000 &
+
+sleep 1
+echo
+
+python manage.py runleecher &
+echo
+
+wait -n
+
+exit $?
