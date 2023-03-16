0e66fe0fdd37466a600b1420c3ba5f33741bba80
Xie Yanbo <xieyanbo@gmail.com>
2023-03-16 14:30:29 +0800

    feature: /task/id/files api (#3)

--- baidupcsleecher/settings.py
+++ baidupcsleecher/settings.py
@@ -53,8 +53,8 @@ INSTALLED_APPS = [
 ]
 
 # baidupcsleecher settings
-DATA_DIR = Path(getenv("DATA_DIR", "/tmp/")).resolve()
-REMOTE_LEECHER_DIR = getenv("REMOTE_LEECHER_DIR", "/leecher/")
+DATA_DIR = Path(getenv("DATA_DIR", "/tmp")).resolve()
+REMOTE_LEECHER_DIR = str(Path(getenv("REMOTE_LEECHER_DIR", "/leecher")).resolve())
 DOWNLOADER_SLEEP_SECONDS = int(getenv("DOWNLOADER_SLEEP_SECONDS", "5"))
 PAN_BAIDU_BDUSS = getenv("PAN_BAIDU_BDUSS", "")
 PAN_BAIDU_COOKIES = getenv("PAN_BAIDU_COOKIES", "")
