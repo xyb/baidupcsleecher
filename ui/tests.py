540bb8cb9df780780565adf09ff2380dae3734b6
Xie Yanbo <xieyanbo@gmail.com>
2023-05-28 13:49:57 +0800

    test: for ui

--- ui/tests.py
+++ ui/tests.py
@@ -1,1 +1,22 @@
-# Create your tests here.
+from django.test import Client
+from django.test import TestCase
+from django.urls import reverse
+
+
+class TaskUITestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        url = reverse("task-list")
+        data = {
+            "shared_link": "https://pan.baidu.com/s/badbeef?pwd=bee",
+        }
+        self.client.post(url, data, format="json")
+
+    def test_task_list(self):
+        url = reverse("index")
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(b"badbeef" in response.content)
+        self.assertTrue(b"bee" in response.content)
